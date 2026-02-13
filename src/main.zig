8b458ef96568884ca7ac4547f2a9dd5c3a7d7389
reumer06 <n0sync.76@gmail.com>
2026-02-14 00:00:16 +0530

    -- hello zig

--- src/main.zig
+++ src/main.zig
@@ -1,11 +1,5 @@
-const c = @cImport({
-    @cInclude("stdio.h");
-    @cInclude("stdlib.h");
-    @cInclude("string.h");
-    @cInclude("time.h");
-    @cInclude("sys/wait.h");
-});
+const std = @import("std");
 
 pub fn main() void {
-    _ = c.printf("the code is this\n");
+    std.debug.print("Hello {s}!\n",.{"World"});
 }
