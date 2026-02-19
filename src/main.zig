40c39d86fb4039cfd16aaa4d7854a32009521791
reumer06 <n0sync.76@gmail.com>
2026-02-19 06:08:25 +0530

    chore: cleanup

--- src/main.zig
+++ src/main.zig
@@ -1,13 +1,5 @@
 const std = @import("std");
-// const print = std.debug.print;
-// const expect = std.testing.expect;
-// const eql = std.mem.eql;
-
 pub fn main() !void {
-    // var gpa = std.heap.GeneralPurposeAllocator(.{}){};
-    // defer _ = gpa.deinit();
-    // const allocator = gpa.allocator();
-
     var stdout_buf: [1024]u8 = undefined;
     var stdout_writer = std.fs.File.stdout().writer(&stdout_buf);
     const stdout: *std.io.Writer = &stdout_writer.interface;
@@ -22,8 +14,6 @@ pub fn main() !void {
 
         const bare_line = try stdin.takeDelimiter('\n') orelse unreachable;
         const line = std.mem.trim(u8, bare_line, "\r");
-        // const owned = try allocator.dupe(u8, line);
-        // defer allocator.free(owned);
         try stdout.print("{s}\n", .{line});
         try stdout.flush();
     }
