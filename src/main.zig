49d6ce7f3ef39cd6b3868b8f8e855a7d115b3930
reumer06 <n0sync.76@gmail.com>
2026-02-19 06:07:35 +0530

    refactor: removed gpa

--- src/main.zig
+++ src/main.zig
@@ -1,7 +1,30 @@
 const std = @import("std");
+// const print = std.debug.print;
+// const expect = std.testing.expect;
+// const eql = std.mem.eql;
 
 pub fn main() !void {
-    var gpa = std.heap.GeneralPurposeAllocator(.{}){}; // allocator object
-    defer _ = gpa.deinit(); // free memory
+    // var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    // defer _ = gpa.deinit();
     // const allocator = gpa.allocator();
+
+    var stdout_buf: [1024]u8 = undefined;
+    var stdout_writer = std.fs.File.stdout().writer(&stdout_buf);
+    const stdout: *std.io.Writer = &stdout_writer.interface;
+
+    var stdin_buf: [1024]u8 = undefined;
+    var stdin_reader = std.fs.File.stdin().reader(&stdin_buf);
+    const stdin: *std.io.Reader = &stdin_reader.interface;
+
+    while (true) {
+        try stdout.writeAll("> ");
+        try stdout.flush();
+
+        const bare_line = try stdin.takeDelimiter('\n') orelse unreachable;
+        const line = std.mem.trim(u8, bare_line, "\r");
+        // const owned = try allocator.dupe(u8, line);
+        // defer allocator.free(owned);
+        try stdout.print("{s}\n", .{line});
+        try stdout.flush();
+    }
 }
