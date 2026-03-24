2361a25cb01163c3e1aa39315731ee80621bae76
reumer06 <n0sync.76@gmail.com>
2026-03-25 01:11:43 +0530

    feat: add scoop extras

--- src/scripts.ps1
+++ src/scripts.ps1
@@ -1,10 +1,13 @@
-Write-Host "Intializing zhell..."
+Write-Host "Initializing zhell..."
 
 $scoopCmd = Get-Command scoop -ErrorAction SilentlyContinue
 
 if ($null -eq $scoopCmd) {
     Write-Host "Scoop not found. Installing Scoop..."
     irm get.scoop.sh | iex
+    Write-Host "Installed Scoop successfully.`nAdding Extras..."
+    scoop bucket add versions
+    scoop bucket add extras
 } else {
     Write-Host "Found Scoop."
 }
