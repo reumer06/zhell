2b829eb71d0eb897a4edeeb0b1ad71cd548a5586
reumer06 <n0sync.76@gmail.com>
2026-03-25 01:20:32 +0530

    style: code formatting

--- src/scripts.ps1
+++ src/scripts.ps1
@@ -5,9 +5,7 @@ $scoopCmd = Get-Command scoop -ErrorAction SilentlyContinue
 if ($null -eq $scoopCmd) {
     Write-Host "Scoop not found. Installing Scoop..."
     irm get.scoop.sh | iex
-    Write-Host "Installed Scoop successfully.`nAdding Extras..."
-    scoop bucket add versions
-    scoop bucket add extras
-} else {
+}
+else {
     Write-Host "Found Scoop."
 }
